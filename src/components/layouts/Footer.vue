2575db77895f6cfb180ebdc35a731ea75d396ea3
sonphfs <sonph@kaopiz.com>
2019-10-22 13:34:33 +0700

    create main component

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- footer content -->
+    <footer>
+      <div class="pull-right">
+        Gentelella - Bootstrap Admin Template by
+        <a href="https://colorlib.com">Colorlib</a>
+      </div>
+      <div class="clearfix"></div>
+    </footer>
+    <!-- /footer content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
---
+++ src/components/layouts/SidebarLeft.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+      <div class="navbar nav_title" style="border: 0;">
+        <a href="index.html" class="site_title">
+          <i class="fa fa-paw"></i>
+          <span style="padding-left: 10px;">EN-C System</span>
+        </a>
+      </div>
+
+      <div class="clearfix"></div>
+
+      <!-- menu profile quick info -->
+      <div class="profile clearfix">
+        <div class="profile_pic">
+          <img src="@/assets/images/user.png" alt="..." class="img-circle profile_img" />
+        </div>
+        <div class="profile_info">
+          <span>Welcome,</span>
+          <h2>Son PH</h2>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+      <!-- /menu profile quick info -->
+
+      <br />
+
+      <!-- sidebar menu -->
+      <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+        <div class="menu_section">
+          <h3>General</h3>
+          <ul class="nav side-menu">
+            <li class="active">
+              <a>
+                <i class="fa fa-home"></i> Users Management
+                <span class="fa fa-chevron-down"></span>
+              </a>
+              <ul class="nav child_menu" style="display: block">
+                <li>
+                  <a href="index.html">Dashboard</a>
+                </li>
+                <li>
+                  <a href="index2.html">Dashboard2</a>
+                </li>
+                <li>
+                  <a href="index3.html">Dashboard3</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>
+                <i class="fa fa-edit"></i> Examination Management
+                <span class="fa fa-chevron-down"></span>
+              </a>
+              <ul class="nav child_menu" style="display: block">
+                <li>
+                  <a href="form.html">General Form</a>
+                </li>
+                <li>
+                  <a href="form_advanced.html">Advanced Components</a>
+                </li>
+                <li>
+                  <a href="form_validation.html">Form Validation</a>
+                </li>
+                <li>
+                  <a href="form_wizards.html">Form Wizard</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>
+                <i class="fa fa-desktop"></i> Learning Words
+                <span class="fa fa-chevron-down"></span>
+              </a>
+              <ul class="nav child_menu" style="display: block">
+                <li>
+                  <a href="general_elements.html">General Elements</a>
+                </li>
+                <li>
+                  <a href="media_gallery.html">Media Gallery</a>
+                </li>
+                <li>
+                  <a href="typography.html">Typography</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>
+                <i class="fa fa-table"></i> Lessons Management
+                <span class="fa fa-chevron-down"></span>
+              </a>
+              <ul class="nav child_menu" style="display: block">
+                <li>
+                  <a href="tables.html">Tables</a>
+                </li>
+                <li>
+                  <a href="tables_dynamic.html">Table Dynamic</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>
+                <i class="fa fa-bar-chart-o"></i> Questions Management
+                <span class="fa fa-chevron-down"></span>
+              </a>
+              <ul class="nav child_menu" style="display: block">
+                <li>
+                  <a href="chartjs.html">Chart JS</a>
+                </li>
+                <li>
+                  <a href="chartjs2.html">Chart JS2</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- /sidebar menu -->
+
+      <!-- /menu footer buttons -->
+      <div class="sidebar-footer hidden-small">
+        <a data-toggle="tooltip" data-placement="top" title="Settings">
+          <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+        </a>
+        <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+          <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+        </a>
+        <a data-toggle="tooltip" data-placement="top" title="Lock">
+          <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+        </a>
+        <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+          <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+        </a>
+      </div>
+      <!-- /menu footer buttons -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
